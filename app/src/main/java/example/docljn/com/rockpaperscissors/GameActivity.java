f476b699fbf0bdfb2fa21c9758098179d1c64049
DocLJN <ljnoble@gmail.com>
2018-01-24 14:34:12 +0000

    result display text fields created.

--- app/src/main/java/example/docljn/com/rockpaperscissors/GameActivity.java
+++ app/src/main/java/example/docljn/com/rockpaperscissors/GameActivity.java
@@ -2,12 +2,35 @@ package example.docljn.com.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
+
+    Game game;
+    Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        game = new Game();
+        player = game.getPlayer();
+
+    }
+
+    public void onRockButtonClicked(View button){
+        String playerChoice = "Rock";
+        player.choose(playerChoice);
+        String result = game.getResult();
+
+        String computerChoice = game.getHand();
+
+
+        
+
+
+
+
     }
 }
