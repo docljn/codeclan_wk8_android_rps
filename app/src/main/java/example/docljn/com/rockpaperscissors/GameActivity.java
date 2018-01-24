302d56a19e077c81e225dcced85d1a0a99c1097f
DocLJN <ljnoble@gmail.com>
2018-01-24 22:14:01 +0000

    Game Activity refactored to include new Game methods, game records score.

--- app/src/main/java/example/docljn/com/rockpaperscissors/GameActivity.java
+++ app/src/main/java/example/docljn/com/rockpaperscissors/GameActivity.java
@@ -1,64 +1,49 @@
-//package example.docljn.com.rockpaperscissors;
-//
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.TextView;
-//
-//public class GameActivity extends AppCompatActivity {
-//
-//    Game game;
-//    String result;
-//    String computerChoice;
-//    String playerChoice;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_game);
-//        // TODO: think about whether this should be in each buttonclick bit
-//        game = new Game();
-//
-//    }
-//
-//
-//
-//    public void displayResults(){
-//        TextView playerChoiceText = findViewById(R.id.player_choice_text);
-//        playerChoiceText.setText("Player chose " + playerChoice);
-//
-//        TextView computerChoiceText = findViewById(R.id.computer_choice_text);
-//        computerChoiceText.setText("Computer chose " + computerChoice);
-//
-//        TextView resultText = findViewById(R.id.result_text);
-//        resultText.setText(result);
-//    }
-//
-//
-//
-//    public void onRockButtonClicked(View button){
-//        playerChoice = "Rock";
-//        player.choose(playerChoice);
-//        result = game.getResult();
-//        computerChoice = game.getHand();
-//        this.displayResults();
-//    }
-//
-//    public void onPaperButtonClicked(View button){
-//        playerChoice = "Paper";
-//        player.choose(playerChoice);
-//        result = game.getResult();
-//        computerChoice = game.getHand();
-//        this.displayResults();
-//    }
-//
-//    public void onScissorsButtonClicked(View button){
-//        playerChoice = "Scissors";
-//        player.choose(playerChoice);
-//        result = game.getResult();
-//        computerChoice = game.getHand();
-//        this.displayResults();
-//    }
-//
-//
-//}
+package example.docljn.com.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    Game game;
+    String result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        game = new Game();
+    }
+
+    public void onRockButtonClicked(View button){
+        result = game.play("Rock");
+        this.displayResults();
+    }
+
+    public void onPaperButtonClicked(View button){
+        result = game.play("Paper");
+        this.displayResults();
+    }
+
+    public void onScissorsButtonClicked(View button){
+        result = game.play("Scissors");
+        this.displayResults();
+    }
+
+
+
+    public void displayResults(){
+        TextView playerChoiceText = findViewById(R.id.player_choice_text);
+        playerChoiceText.setText("Player chose " + game.getPlayerHand());
+
+        TextView computerChoiceText = findViewById(R.id.computer_choice_text);
+        computerChoiceText.setText("Computer chose " + game.getComputerHand());
+
+        TextView resultText = findViewById(R.id.result_text);
+        resultText.setText(result);
+    }
+
+
+}
