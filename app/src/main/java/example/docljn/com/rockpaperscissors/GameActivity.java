0ad4a7ef0aae6df6104a569d5ba47b1ae123a84a
DocLJN <ljnoble@gmail.com>
2018-01-24 14:53:32 +0000

    Rock button displays results.

--- app/src/main/java/example/docljn/com/rockpaperscissors/GameActivity.java
+++ app/src/main/java/example/docljn/com/rockpaperscissors/GameActivity.java
@@ -3,11 +3,15 @@ package example.docljn.com.rockpaperscissors;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
     Game game;
     Player player;
+    String result;
+    String computerChoice;
+    String playerChoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +23,28 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-    public void onRockButtonClicked(View button){
-        String playerChoice = "Rock";
-        player.choose(playerChoice);
-        String result = game.getResult();
-
-        String computerChoice = game.getHand();
 
 
-        
+    public void displayResults(){
+        TextView playerChoiceText = findViewById(R.id.player_choice_text);
+        playerChoiceText.setText("Player chose " + playerChoice);
 
+        TextView computerChoiceText = findViewById(R.id.computer_choice_text);
+        computerChoiceText.setText("Computer chose " + computerChoice);
 
-
-
+        TextView resultText = findViewById(R.id.result_text);
+        resultText.setText(result);
     }
+
+
+
+    public void onRockButtonClicked(View button){
+        playerChoice = "Rock";
+        player.choose(playerChoice);
+        result = game.getResult();
+        computerChoice = game.getHand();
+        this.displayResults();
+    }
+
+
 }
